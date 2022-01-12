ba1fb390c8b87263ef73238b2313cc7ce57715d2
yasirabd <sir.yasirabd@gmail.com>
2022-01-12 18:17:21 +0700

    update region to ap-southeast-3

--- src/api_v1/api.py
+++ src/api_v1/api.py
@@ -7,7 +7,7 @@ from api_v1.estimate.vbm import VBM
 router = APIRouter()
 
 @router.get("/estimate/")
-async def estimate_sensor(actual: list, date: str, state_matrix: str):
+async def estimate_sensor(actual: float, date: str, state_matrix: str):
     # load state matrix
     state_m = np.load('src/state_matrix.npy')
     actual = np.array([594.6552, 46.3567619, 79.52576, 84.71144, 1.288945, 51.4658165])
