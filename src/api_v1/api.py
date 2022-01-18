23c2abcc035b49a695bf83e8b9ab39e1e4b342aa
yasirabd <sir.yasirabd@gmail.com>
2022-01-19 06:03:45 +0700

    fixed endpoint /estimates/

--- src/api_v1/api.py
+++ src/api_v1/api.py
@@ -1,20 +1,22 @@
 import numpy as np
+from typing import List
+import os
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query, Body
 from api_v1.estimate.vbm import VBM
 
 
 router = APIRouter()
 
-@router.get("/estimate/")
-async def estimate_sensor(actual: float, date: str, state_matrix: str):
+@router.get("/estimates/")
+async def estimate_sensor(actual: float = 10., date: str = 'date', state_matrix: str = 'path'):
     # load state matrix
-    state_m = np.load('src/state_matrix.npy')
-    actual = np.array([594.6552, 46.3567619, 79.52576, 84.71144, 1.288945, 51.4658165])
+    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'state_matrix.npy')
+    state_m = np.load(path)
+    actuals = np.array([594.6552, 46.3567619, 79.52576, 84.71144, 1.288945, 51.4658165])
 
     vbm = VBM()
-    dynamic_m, weight = vbm.create_dynamic_matrix(state_m, actual)
+    dynamic_m, weight = vbm.create_dynamic_matrix(state_m, actuals)
     estimate = vbm.estimate_value(dynamic_m, weight)
-    print(estimate)
 
-    return {"message": "success"}
+    return {"message": "success", "estimate": list(estimate)}
